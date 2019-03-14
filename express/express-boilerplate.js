4349b6ba98d21fb1ff2e5cea1b1f19a67662aec7
hayden7913 <hayden321@gmail.com>
2019-03-13 18:20:48 -0700

    removes cors

--- express/express-boilerplate.js
+++ express/express-boilerplate.js
@@ -1,12 +1,10 @@
 // yarn add express cors body-parser
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
