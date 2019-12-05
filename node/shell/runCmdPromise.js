a15c8ee1b77f3fc0ab2d26f9773662cad4d1d9ad
hayden7913 <hayden321@gmail.com>
2019-12-04 16:35:55 -0800

    converst to promise

--- node/shell/runCmdPromise.js
+++ node/shell/runCmdPromise.js
@@ -1,12 +1,16 @@
 const { exec } = require('child_process');
 // pass any bash command to this function wrapped in a quotes
 const runCmd = (cmd) => {
-  exec(cmd, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(stdout);
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+        return;
+      }
+      console.log(stdout);
+      resolve();
+    });
   });
 };
 
